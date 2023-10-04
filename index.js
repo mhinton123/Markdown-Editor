a2007846f21b2daa9b58aab4fa3628d6035a33f8
mhinton123 <matthewhinton@hotmail.co.uk>
2023-10-04 17:28:34 +0100

    CSS fixed and first h1 rendered

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const menuIconEl = document.getElementById("hdr-menu-icon")
 const sidebarDivEl = document.getElementById("sb-wr")
 const markdownInputEl = document.getElementById("markdown-input")
+const previewDivEl = document.getElementById("preview-content")
 
+let previewHtml = ``
+
+// Markdown input
 markdownInputEl.addEventListener("input", function(e){
+    
+    previewHtml = ``
     
     const markdownText = e.target.value
     parseMarkdownText(markdownText)
+    
+    console.log(previewHtml)
+    render()
 
+})
+
+// Btn click
+document.addEventListener("click", function(e) {
+    if ( e.target === menuIconEl ) {
+        handleMenuBtn()
+    }
 })
 
 function parseMarkdownText(markdownText) {
     
     const textTolineArr = markdownText.split('\n')
-    const previewHtml = ``
     let isCode = false
     
     textTolineArr.forEach(line => {
-        
-        
-        
+             
         // Check for headings
         if (line.startsWith("# ")) {
-            console.log("The string starts with '# '.");
+            formatLineToHtml("h1", line)
         } 
         else if (line.startsWith("## ")){
             console.log("The string starts with '## '.");
@@ -53,40 +66,39 @@ function parseMarkdownText(markdownText) {
         else if (line.startsWith("> ")){
             console.log("The string starts with '> '.");
         }
-        // Check for code block
+        // Check for start of a code block
         else if (line.startsWith("```") && isCode === false){
             console.log("The string starts with '```'.");
             isCode = true
         }
+        // Check for end of a code block
         else if (line.startsWith("```") && isCode === true){
             console.log("The string starts with '```'.");
             isCode = false
         }
+        // Check for line of code
         else if (isCode === true){
             console.log(line + " is a line of code")
         }
+        // Check for <br>
         else if (line === ''){
             console.log("<br>")
         }
+        // Anything else is body text
         else{
             console.log("this is body text")
         }
 
-          
-          
-          
-          
-    
     })
 }
 
-document.addEventListener("click", function(e) {
-    if ( e.target === menuIconEl ) {
-        handleMenuBtn()
+function formatLineToHtml(element, innerText) {
+
+    if ( element === "h1" ) {
+        previewHtml += `<h1 class="p-h1">${innerText}</h1>`
     }
-})
 
-
+}
 
 function handleMenuBtn() {
 
@@ -109,6 +121,11 @@ function handleMenuBtn() {
         menuIconEl.src = "/assets/icon-menu.svg"
     
     }
-    
-    
+
 }
+
+function render() {
+    previewDivEl.innerHTML = previewHtml
+}
+
+render()
